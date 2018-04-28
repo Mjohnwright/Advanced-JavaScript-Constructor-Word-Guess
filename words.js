3750a24bed1aada34827131371da04a49bd9747c
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-04-27 21:15:14 -0400

    first actual submission

---
+++ words.js
@@ -0,0 +1,25 @@
+// var Letters = require("/letters.js");
+
+
+// function Game() {
+//     this.guessLeft = 10;
+
+//     prompt.get(['guess'], function (err, result) {
+//         // Log the results. 
+//         console.log('Command-line input received:');
+//         console.log('  guess   : ' + result.guess);
+    
+//         //this var is reassigned to make it more workable
+//         var guess = result.guess;
+//         console.log('GUESS IS LINE 17: '+ guess)
+    
+//         //this var is the secret word for testing purposes
+//         var secretWord = "x";
+        
+    
+       
+        
+//     });
+// }
+
+// Game();
